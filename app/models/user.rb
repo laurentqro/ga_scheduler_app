d9ac0b08d8b64a19c5c814358d4498cfb505b5a6
Laurent Curau <laurentcurau@gmail.com>
2014-02-04 22:46:31 +0000

    Set up HABTM association between users and roles

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ActiveRecord::Base
-  attr_accessible :email, :first_name, :last_name
+  attr_accessible :email, :first_name, :last_name, :role_ids
+  has_and_belongs_to_many :roles
 end
