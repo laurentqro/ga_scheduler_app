09f2d49821198b0aa558a194a14f1895b8830442
Laurent Curau <laurentcurau@gmail.com>
2014-02-04 15:36:13 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GaSchedulerApp::Application.config.secret_token = '3d5d1cc43eee5f169201c1c93da488eed0b15d0742c6068665ab5ed60a6637f99f0405c35fe9b726775915b1f9b031755a10bbb77beb74de9310529a4c2cfbb1'
