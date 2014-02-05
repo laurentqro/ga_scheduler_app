e37f748636aa422c3985b582b3c45e3106429309
Laurent Curau <laurentcurau@gmail.com>
2014-02-05 15:48:45 +0000

    Associate classrooms and courses

--- app/models/classroom.rb
+++ app/models/classroom.rb
@@ -1,3 +1,4 @@
 class Classroom < ActiveRecord::Base
-  attr_accessible :capacity, :location, :name
+  attr_accessible :capacity, :location, :name, :course_ids
+  has_many :courses
 end
--- app/models/course.rb
+++ app/models/course.rb
@@ -1,4 +1,8 @@
 class Course < ActiveRecord::Base
-  attr_accessible :date, :description, :name, :time_slot_id
+  attr_accessible :date, :description, :name, :time_slot_id, :classroom_id, :user_ids
   belongs_to :time_slot
+  belongs_to :classroom
+  has_many :enrollments
+  has_many :users, through: :enrollments
+
 end
