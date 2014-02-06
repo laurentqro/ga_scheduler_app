b49e109670264ae6e6ef8da1f1fd3e650c74980f
Laurent Curau <laurentcurau@gmail.com>
2014-02-06 16:44:07 +0000

    Add method to check if course lasts only one day

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,8 +1,12 @@
 class Course < ActiveRecord::Base
-  attr_accessible :date, :description, :name, :time_slot_id, :classroom_id, :user_ids
+  attr_accessible :date, :description, :name, :time_slot_id, :start_date, :end_date, :classroom_id, :user_ids
   belongs_to :time_slot
   belongs_to :classroom
   has_many :enrollments
   has_many :users, through: :enrollments
+
+  def only_one_day?
+    self.start_date == self.end_date
+  end
 
 end
